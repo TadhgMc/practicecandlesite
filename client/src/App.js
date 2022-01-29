fd15ef00f15f9ded987c299527acca125dd4c947
Tadhg McIntyre <mcintyretadhg@gmail.com>
2022-01-29 18:35:13 -0500

    adding base React app outline

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<>
+    <div>Super Cool Candle Co.</div>
+    <div> News Letter</div>
+    </>
+  );
+}
+
+export default App;
